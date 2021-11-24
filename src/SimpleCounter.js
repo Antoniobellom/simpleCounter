434b2754d1bce73b46c04ce01cb158f42f317d9b
Antoniobellom <61624925+Antoniobellom@users.noreply.github.com>
2021-11-24 11:11:29 -0300

    agregando icono

---
+++ src/SimpleCounter.js
@@ -0,0 +1,17 @@
+import React from ("react");
+
+export const SimpleCounter = ()=>{
+    return(
+        <div className="bigContainer">
+            <div className = "calendar">
+            <i className="fas fa-clock"></i>
+            </div>
+            <div className = "four">0</div>
+            <div className = "three">0</div>
+            <div className = "two">0</div>
+            <div className = "one">0</div>
+
+        </div>
+    )
+
+}
--- src/index.js
+++ src/index.js
@@ -3,15 +3,53 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import PropTypes from 'prop-types';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+const SimpleCounter = (props) => {
+  return (<div className="bigCounter">
+
+              <div> <i class="fas fa-clock"></i> </div>
+            <div className = "four">{props.digitFour %10}</div>
+            <div className = "three">{props.digitThree %10}</div>
+            <div className = "two">{props.digitTwo %10}</div>
+            <div className = "one">{props.digitOne %10}</div>
+
+
+            </div>)
+}
+SimpleCounter.propTypes ={
+  digitOne: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitFour: PropTypes.number
+
+};
+let counter = 0
+setInterval(function(){
+  const four = Math.floor(counter/1000);
+  const three = Math.floor(counter/100);
+  const two = Math.floor(counter/10);
+  const one = Math.floor(counter/1);
+  
+  counter ++
+
+  ReactDOM.render(
+    <>
+      <SimpleCounter digitOne={one}
+      digitTwo={two}
+      digitThree={three}
+      digitFour={four} />
+    </>,
+    document.getElementById('root')
+  );
+  
+
+},1000);
+
+
+
 reportWebVitals();
